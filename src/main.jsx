2094aa39b1a5ad53f2cccb1d6764782652c7e296
Joseph-pai <a18911289122@gmail.com>
2026-01-30 21:47:02 +0800

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
+import { SoundProvider } from './context/SoundContext'
+
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <AuthProvider>
-            <App />
+            <SoundProvider>
+                <App />
+            </SoundProvider>
         </AuthProvider>
     </StrictMode>,
 )
